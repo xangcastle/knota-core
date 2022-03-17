d77249a1ee49de5ad7b41f0d3bcbaf11e312e131
Oscar <oscar.javier.marin.robleto@gmail.com>
2022-03-16 19:24:08 -0600

    profile component completed

--- components/ContactAvatar.js
+++ components/ContactAvatar.js
@@ -1,7 +1,38 @@
 import React from "react";
 
-export default function (props){
-    return <div></div>
+const colors = [
+  "#16a085",
+  "#27ae60",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857",
+];
+
+const randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+
+const extractInitials = (string) =>
+  string
+    .split(" ")
+    .map((s) => s[0].toUpperCase())
+    .join("")
+    .substring(0, 2);
+
+export default function ({ contact }) {
+  return (
+    <div
+      className="profileContainer"
+      style={{ backgroundColor: randomColor() }}
+    >
+      <div className="profileText">{extractInitials(contact.name)}</div>
+    </div>
+  );
 }
 
 // oscar

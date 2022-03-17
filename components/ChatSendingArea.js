4e203e6781daae083ac78e64eab4c3b52c93eeba
César Abel Ramírez <cesar.ramirez@gtl.net>
2022-03-16 21:03:23 -0600

    component-chat-sending-area

--- components/ChatSendingArea.js
+++ components/ChatSendingArea.js
@@ -3,9 +3,9 @@ import Button from "./Button";
 import ChatTextMaker from "./ChatTextMaker";
 
 export default function (props){
-    return <div>
+    return <div className={'ChatSendingArea'}>
         <Button className={'sendButton'} />
-        <ChatTextMaker className={'chatTextMaker'} />
+        <ChatTextMaker />
         <Button className={'additionaOptionsButton'} text={'enviar'} onClick={()=>alert('algo')} />
     </div>
 }
--- components/ContactName.js
+++ components/ContactName.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ContactAvatar from "./ContactAvatar";
 
 
 export default function ({contact}){
@@ -12,14 +13,8 @@ export default function ({contact}){
             padding:15,
             justifyContent:'flex-start',
             alignItems:'center'}}>
-            <div style={{
-            borderColor:'black', borderStyle:'solid', backgroundColor:'black',
-            borderWidth:'1px',
-            width:'50px',
-            height:'50px',
-            borderRadius:100}
-            }/>
-            <div style={{marginLeft:'50px'}}>
+            <ContactAvatar contact={contact}/>
+            <div style={{marginLeft:'50px', fontSize: '2em', color: '#4473d9'}}>
                 {contact.name}
             </div>
         </div>
